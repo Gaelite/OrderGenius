40d910a62c16919d4ef8c611befd5d0bbcea7e25
itsvaalentine <96605134+itsvaalentine@users.noreply.github.com>
2024-07-20 16:00:14 -0600

    fromat for every html

--- order-genius/app/layout.tsx
+++ order-genius/app/layout.tsx
@@ -10,15 +10,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="/globals.css" />
-      </head>
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
+    <div className={inter.className}>
+      {children}
+    </div>
   );
 }
